796997534de8393c8340b1f6320bace83cc76712
sbjordal <stine.bjordal@gmail.com>
2025-10-22 15:48:23 +0200

    Satt opp database i supabase og skrevet typedefinisjoner for Task, List og User

---
+++ app/lib/definitions.ts
@@ -0,0 +1,27 @@
+export type Task = {
+    id: string;
+    listId: string; //koblet til listen oppgaven tilhører
+    title: string;
+    completed: boolean;
+    dueDate?: string | null;
+    tags?: string[];
+    createdAt: string;
+    updatedAt?: string | null;
+}
+
+export type List = {
+    id: string;
+    userId: string;
+    name: string;
+    type: string; //legger til rette for flere listetyper (skole, jobb, handleliste, gaveliste, etc)
+    createdAt: string;
+}
+
+export type User = { //ønsker å ha flere brukere etterhvert
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: string;
+}
+
