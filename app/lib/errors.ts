53c671842171c8bde4a2414a8df0d075052e7937
sbjordal <stine.bjordal@gmail.com>
2025-11-03 08:29:21 +0100

    Angret sletting av error.ts, fikset feilmelding

---
+++ app/lib/errors.ts
@@ -0,0 +1,9 @@
+export class AppError extends Error {
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(JSON.stringify({ status, message }));
+    this.status = status;
+    this.name = "AppError";
+  }
+}
